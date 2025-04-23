6f4386b59ecbb28764ab418013834fc5eace5b1d
Morris235 <dltkdah777@gmail.com>
2025-04-23 12:24:21 +0900

    chore : first init for docker test

---
+++ src/test/java/com/pive/springbe/SpringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pive.springbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
