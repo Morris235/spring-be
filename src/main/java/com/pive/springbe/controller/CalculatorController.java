00177b9e1acbe2abf7679096efbe7aa91ac025cd
Morris235 <dltkdah777@gmail.com>
2025-04-24 12:41:21 +0900

    chore : add dockerfile

---
+++ src/main/java/com/pive/springbe/controller/CalculatorController.java
@@ -0,0 +1,38 @@
+package com.pive.springbe.controller;
+
+import com.pive.springbe.dto.CalculatorDTO;
+import com.pive.springbe.service.CalculatorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class CalculatorController {
+    CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive!!";
+    }
+
+    @PostMapping("/plus")
+    public ResponseEntity<CalculatorDTO> plusTwoNumbers(@RequestBody CalculatorDTO calculatorDTO) {
+        int result = calculatorService.plusTwoNumbers(calculatorDTO);
+        calculatorDTO.setSum(result);
+        log.info("calculatorDTO: {}", calculatorDTO);
+
+        return ResponseEntity
+                .ok()
+                .body(calculatorDTO);
+    }
+}
---
+++ src/main/java/com/pive/springbe/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.pive.springbe.service;
+
+import com.pive.springbe.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
